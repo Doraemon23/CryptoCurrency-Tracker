ad1d8491a4b06399b0711bcfcaeda3ff79b73f13
jagdishsindhi <72116803+jagdishsindhi@users.noreply.github.com>
2022-05-21 10:19:49 +0530

    test2

--- components/HomeScreen/index.jsx
+++ components/HomeScreen/index.jsx
@@ -1,22 +1,57 @@
-import React from 'react';
-import { FlatList, StyleSheet, View, Text } from "react-native";
+import React,{useEffect,useState} from 'react';
+import { FlatList, StyleSheet, View,RefreshControl } from "react-native";
 import CoinItem from "../CoinItem";
-import cryptocurrencies from "../../assets/data/cryptocurrencies.json";
+import { getMarketData } from "../../services/requests";
 
-export default function HomeScreen(){
-    return(
-        <View style={styles.cont1}>
-            <FlatList
-          data={cryptocurrencies}
-          renderItem={({ item }) => <CoinItem coin={item} />}
-        />
-        </View>
-    )
+const HomeScreen = () => {
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchCoins = async (pageNumber) => {
+    if (loading) {
+      return;
+    }
+    setLoading(true);
+    const coinsData = await getMarketData(pageNumber);
+    setCoins((existingCoins) => [...existingCoins, ...coinsData]);
+    setLoading(false);
+  };
+
+  const refetchCoins = async () => {
+    if (loading) {
+      return;
+    }
+    setLoading(true);
+    const coinsData = await getMarketData();
+    setCoins(coinsData);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchCoins();
+  }, []);
+  return (
+    <View style={styles.cont1}>
+      <FlatList
+        data={coins}
+        renderItem={({ item }) => <CoinItem coin={item} />}
+        onEndReached={() => fetchCoins(coins.length / 50 + 1)}
+        refreshControl={
+          <RefreshControl
+            refreshing={loading}
+            tintColor="white"
+            onRefresh={refetchCoins}
+          />
+        }
+      />
+    </View>
+  )
 }
 const styles = StyleSheet.create({
-    cont1: {
-      flex: 1,
-      backgroundColor: "black",
-      paddingTop: 50,
-    },
-  });
+  cont1: {
+    flex: 1,
+    backgroundColor: "black",
+    paddingTop: 50,
+  },
+});
+export default HomeScreen;
